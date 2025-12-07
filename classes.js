42bcd507ef1d94f3d6d80f3d8210408f47da0cc3
Franco-Bruno <brunoqueiroz313@gmail.com>
2025-12-07 11:40:18 -0300

    trabalhando com classes e objetos

---
+++ classes.js
@@ -0,0 +1,24 @@
+class hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    attack(){
+       
+        if(this.tipo === 'Mago'){
+            console.log(`O ${this.tipo} atacou usando magia.`);
+        } else if(this.tipo === 'Guerreiro'){
+            console.log(`O ${this.tipo} atacou usando espada.`);
+        } else if(this.tipo === 'Monge'){
+            console.log(`O ${this.tipo} atacou usando artes marciais.`);
+        } else if(this.tipo === 'Ninja'){
+            console.log(`O ${this.tipo} atacou usando shuriken.`);
+        } else {
+            console.log('Nenhum herói foi selecionado...');
+        }
+    }
+}
+
+let heroSelected = new hero('Jack Chan', 40, 'Mago');
+heroSelected.attack();
